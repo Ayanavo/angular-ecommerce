78ad40f878aad0a5298a5f93031e4829041b23b5
Ayanavo <ayanavo2057@gmail.com>
2022-05-07 18:22:42 +0530

    2 files modified

--- src/app/route.module.ts
+++ src/app/route.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './modules/page-module/about/about.component';
+import { ContactComponent } from './modules/page-module/contact/contact.component';
 import { HomeComponent } from './modules/page-module/home/home.component';
+import { MenuComponent } from './modules/page-module/menu/menu.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'menu', component: MenuComponent },
+];
 
 @NgModule({
   imports: [RouteModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class RouteModule {
-}
+export class RouteModule {}
